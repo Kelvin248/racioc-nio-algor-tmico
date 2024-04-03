95f908f1629a65cfdf001eb9b13f785ab0aae7c2
Kelvin248 <kelvincabralribas@gmail.com>
2024-04-03 10:38:10 -0300

    calc 2.2

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,9 @@
 status = "c"
 
 while status == "c":
-    oper = str(input("[+] soma [-] subtração \n[/] divisão [*] multiplicação \n[s] sair \n"))
-    	if oper == "s":
+	oper = str(input("[+] soma [-] subtração \n[/] divisão [*] multiplicação \n[s] sair \n"))
+    	
+	if oper == "s":
         	break
     	val1 = float(input("?"))
     	val2 = float(input("??"))
