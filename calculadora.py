ebb9a6ddfd73ce0a5a72e192dbe9d1cc7327c9a7
Kelvin Cabral Ribas <kelvin.cabral@pucpr.edu.br>
2024-04-03 10:25:35 -0300

    ajustand

--- calculadora.py
+++ calculadora.py
@@ -2,23 +2,18 @@ status = "c"
 
 while status == "c":
 	oper = str(input("[+] soma [-] subtração \n[/] divisão [*] multiplicação \n[s] sair \n"))
-	if oper == "+":
-		val1 = float(input("?"))
-		val2 = float(input("??"))
+    if oper == "s":
+        break
+    val1 = float(input("?"))
+    val2 = float(input("??"))
+    
+    elif oper == "+":
 		print(val1 + val2)
 	elif oper == "-":
-		val1 = float(input("?"))
-		val2 = float(input("??"))
 		print(val1 - val2)
 	elif oper == "/":
-		val1 = float(input("?"))
-		val2 = float(input("??"))
 		print(val1 / val2)
 	elif oper == "*":
-		val1 = float(input("?"))
-		val2 = float(input("??"))
 		print(val1 * val2)
-	elif oper == "s":
-		status = "s"
 	else:
 		print("ERROR")
