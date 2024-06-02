e7db460f0f3c5b72a6a9e2e379bfe8c12c48a607
Kelvin248 <kelvincabralribas@gmail.com>
2024-06-02 14:19:41 -0300

    mecanica de luta básica

--- functions.py
+++ functions.py
@@ -1,1 +1,88 @@
-*
+import pygame
+
+class character:
+    def __init__(self,life,surface, vel, x,y):
+        self.rotate = False
+        self.life = 100
+        self.speed = vel
+        self.rect = pygame.Rect((x,y,80,180))
+        self.surface = surface
+        self.vel_y = 0
+        self.jump = True
+        self.attack_grid = True
+        self.attack_type = 0
+        
+    
+    def move(self,up,down,left,right, attack1,attack2,target):
+        key = pygame.key.get_pressed()
+
+        dx = 0
+        dy = 0
+        
+        gravity = 1
+        #controles
+
+        if key[left]:
+            dx -= self.speed
+        if key[right]:
+            dx +=self.speed
+        if key[up] and self.jump:
+            self.vel_y -= 25
+            self.jump = False
+
+        #Rotacionando personagens:
+        if target.rect.centerx > self.rect.centerx:
+            self.rotate = False
+        else:
+            self.rotate = True
+
+        #gravidade
+        self.vel_y += gravity
+        dy += self.vel_y 
+
+        #limites da tela
+
+        if self.rect.left + dx <0:
+            dx = -self.rect.left
+        if self.rect.right + dx > 1200:
+            dx = 1200 - self.rect.right
+        if self.rect.bottom + dy > 670:
+            self.vel_y = 0
+            dy = 670 - self.rect.bottom
+            self.jump = True
+        
+        if self.attack_grid:
+            if key[attack1] or key[attack2]:
+            
+                if key[attack1]:
+                    self.attack(target,punch=True, kick=False)
+                    self.attack_type = 1
+                if key[attack2]:
+                    self.attack(target, punch=False,kick=True)
+                    self.attack_type = 2
+        
+        
+        
+        #atualizando posição do personagem
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def attack(self,target, punch, kick):
+        if punch == True:
+            punch_attack = pygame.Rect(self.rect.centerx -(1.5* self.rect.width * self.rotate) ,self.rect.y, 1.5*self.rect.width,self.rect.height/2)
+            pygame.draw.rect(self.surface,'green',punch_attack)
+            if punch_attack.colliderect(target):
+                target.life -= 10
+                self.attack_grid = False
+                pygame.draw.rect(self.surface,'red',punch_attack)
+        if kick == True:
+            kick_attack = pygame.Rect(self.rect.centerx -(1.5* self.rect.width * self.rotate) ,self.rect.y+90, 1.5*self.rect.width,self.rect.height/2)
+            pygame.draw.rect(self.surface,'green',kick_attack)
+            if kick_attack.colliderect(target):
+                target.life -= 10
+                self.attack_grid = False
+                pygame.draw.rect(self.surface,'red',kick_attack)
+        
+
+    def draw(self, color):
+        pygame.draw.rect(self.surface, color, self.rect)
--- main.py
+++ main.py
@@ -1,1 +1,70 @@
-*
+import pygame
+from functions import *
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((1200,800))
+pygame.display.set_caption("PUC Fight")
+
+#importando sprites
+#idle = pygame.image.load('Knight_1/Idle.png').convert_alpha()
+#attack1 = pygame.image.load('Knight_1/Attack 1.png').convert_alpha()
+
+#background = pygame.image.load('backgrounds/sky bridge.png')
+
+#Animação = [n° frames,]
+ANIMATION_IDLE = [4,]
+
+
+#criando os jogadores
+player_1 = character(100,screen,7,300,400)
+player_2 = character(100,screen,7,500,400)
+
+
+
+#barra de vida dos personagens
+
+def draw_life_bar(life, x, y):
+    ratio = life / 100
+    pygame.draw.rect(screen, 'black',(x -2,y -2 , 403,34))
+    pygame.draw.rect(screen, 'white',(x,y, 400,30))
+    pygame.draw.rect(screen, 'red',(x,y, 400*ratio,30))
+
+
+def enemy_draw_life_bar(life, x, y):
+    ratio = life / 100
+    pygame.draw.rect(screen, 'black',(x -2,y -2 , 403,34))
+    pygame.draw.rect(screen, 'white',(x,y, 400,30))
+    pygame.draw.rect(screen, 'red',(x+(400-(400*ratio)),y, 400*ratio,30))
+
+
+Clock = pygame.time.Clock()
+running =True
+while running == True:
+    screen.fill('gray')
+
+
+    #adicionando movimento e limite na tela
+    player_1.move(pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d, pygame.K_k, pygame.K_l, player_2)
+    player_2.move(pygame.K_UP,pygame.K_DOWN,pygame.K_LEFT,pygame.K_RIGHT, pygame.K_1,pygame.K_2,player_1)
+
+    #desenhando os jogadores na tela
+    player_1.draw('red')
+    player_2.draw('blue')
+
+    #desenhando a vida do jogador
+    draw_life_bar(player_1.life,20,20)
+    enemy_draw_life_bar(player_2.life,780,20)
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    Clock.tick(60)
+    pygame.display.update()
+
+pygame.quit()
