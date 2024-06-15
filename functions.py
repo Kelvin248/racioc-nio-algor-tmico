13b3b689c7617f0a098320d16a81768943a4a563
Kelvin248 <kelvincabralribas@gmail.com>
2024-06-15 13:54:06 -0300

    adicionando sprites temporários

--- functions.py
+++ functions.py
@@ -1,20 +1,39 @@
 import pygame
 
 class character:
-    def __init__(self,life,surface, vel, x,y):
-        self.rotate = False
-        self.life = 100
-        self.speed = vel
-        self.player_higth = 180
-        self.rect = pygame.Rect((x,y,80,self.player_higth))
+    def __init__(self,life, vel, x,y,height,data, sprites, steps, surface):
         self.surface = surface
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.action = 0
+        self.frame_index = 0
+        self.animation_list = self.images_load(sprites, steps)
+        self.image = self.animation_list[self.action][self.frame_index]  
+        self.timer = pygame.time.get_ticks()     
+        self.rotate = False
+        self.life = life
+        self.speed = vel
+        self.player_heigth = height
+        self.rect = pygame.Rect((x,y,80,self.player_heigth))
         self.vel_y = 0
         self.jump = True
         self.attack_grid = True
         self.attack_type = 0
-        
-    # def images_load(self,sprites, steps):
+        self.attack_accept = False
 
+        
+    def images_load(self,sprites, steps):
+        animation_list = []
+        for y, step in enumerate(steps):
+            temp_list = []
+            for x in range(step):
+                temp_img= sprites.subsurface(x*self.size, y*self.size, self.size, self.size)
+                temp_list.append(pygame.transform.scale(temp_img, (self.size*self.image_scale,self.size*self.image_scale)))
+            animation_list.append(temp_list)
+        print(animation_list)
+        return animation_list
+    
 
     def move(self,up,down,left,right, attack1,attack2,target):
         key = pygame.key.get_pressed()
@@ -33,9 +52,7 @@ class character:
             self.vel_y -= 20
             self.jump = False
         if key[down]:
-            self.player_higth = 160
-        else:
-            self.player_higth = 180
+            self.player_heigth *= 0.5
 
 
         #Rotacionando personagens:
@@ -60,20 +77,41 @@ class character:
             self.jump = True
         
         if self.attack_grid:
-            if key[attack1] or key[attack2]:
-            
-                if key[attack1]:
+            if key[attack1] or key[attack2]:        
+                if key[attack1] and not key[attack2]:
                     self.attack(target,punch=True, kick=False)
                     self.attack_type = 1
-                if key[attack2]:
+                    self.attack_accept = True
+                if key[attack2] and not key[attack1]:
                     self.attack(target, punch=False,kick=True)
                     self.attack_type = 2
-        
-        
+                
         
         #atualizando posição do personagem
         self.rect.x += dx
         self.rect.y += dy
+    
+    def update_sprites(self):
+        if self.attack_type == 1 and self.attack_accept:
+            self.update_action(1)
+        else:
+            self.update_action(0)
+        animation_cooldown = 50
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.timer > animation_cooldown:
+            self.frame_index += 1
+            self.timer = pygame.time.get_ticks()
+        if self.frame_index == len(self.animation_list[self.action]):
+            self.frame_index = 0
+            self.attack_accept = False
+            self.attack_grid = True
+        
+    def update_action(self, newaction):
+        #atualizando mudanças de sprites
+        if newaction != self.action:
+            self.action = newaction
+            self.frame_index = 0
+            self.timer = pygame.time.get_ticks()
 
     def attack(self,target, punch, kick):
         if punch == True:
@@ -93,4 +131,6 @@ class character:
         
 
     def draw(self, color):
+        img = pygame.transform.flip(self.image, self.rotate, False)
         pygame.draw.rect(self.surface, color, self.rect)
+        self.surface.blit(img,(self.rect.x - (self.offset[0]*self.image_scale), self.rect.y - (self.offset[1]*self.image_scale)))
--- main.py
+++ main.py
@@ -8,18 +8,19 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("PUC Fight")
 
 #importando sprites
-#idle = pygame.image.load('Knight_1/Idle.png').convert_alpha()
-#attack1 = pygame.image.load('Knight_1/Attack 1.png').convert_alpha()
+IDLE = pygame.image.load('sprites/Fighter/Idle.png').convert_alpha()
 
-#background = pygame.image.load('backgrounds/sky bridge.png')
-
-#Animação = [n° frames,]
-ANIMATION_IDLE = [4,20]
+FIGHTER_SIZE = 128
+FIGHTER_SCALE = 2.3
+FIGHTER_OFFSET = [44,50]
+FIGHER_DATA = [FIGHTER_SIZE, FIGHTER_SCALE, FIGHTER_OFFSET]
+#Animação = [n° frames/linha]
+ANIMATION_IDLE = [6, 4]
 
 
 #criando os jogadores
-player_1 = character(100,screen,7,40,400)
-player_2 = character(100,screen,7,680,400)
+player_1 = character(100,5,40,400, 180, FIGHER_DATA, IDLE,ANIMATION_IDLE, screen)
+player_2 = character(100,5,680,400, 180, FIGHER_DATA, IDLE, ANIMATION_IDLE, screen)
 
 #barra de vida dos personagens
 
@@ -37,6 +38,13 @@ def enemy_draw_life_bar(life, x, y):
     pygame.draw.rect(screen, 'red',(x+(250-(250*ratio)),y, 250*ratio,20))
 
 
+
+
+
+
+
+
+
 Clock = pygame.time.Clock()
 running =True
 while running == True:
@@ -46,6 +54,11 @@ while running == True:
     #adicionando movimento e limite na tela
     player_1.move(pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d, pygame.K_k, pygame.K_l, player_2)
     player_2.move(pygame.K_UP,pygame.K_DOWN,pygame.K_LEFT,pygame.K_RIGHT, pygame.K_1,pygame.K_2,player_1)
+
+
+    #atualizando sprite
+    player_1.update_sprites()
+    player_2.update_sprites()
 
     #desenhando os jogadores na tela
     player_1.draw('red')
